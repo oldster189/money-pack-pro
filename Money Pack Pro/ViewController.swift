0a3ed1e31d2d2a50446ece1719b9029fdb7c086b
Oldster <oldster189@gmail.com>
2017-10-13 15:41:57 +0700

    Initial Commit

---
+++ Money Pack Pro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Money Pack Pro
+//
+//  Created by itthipon wiwatthanasathit on 10/13/2560 BE.
+//  Copyright © 2560 Dev7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
